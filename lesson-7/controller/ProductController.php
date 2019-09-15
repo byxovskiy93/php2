0f05768c53c89f137c5d63b46c9aa46f8c37d296
Stone <byxovskiy93@mail.ru>
2019-09-15 18:41:31 +0300

    lesson-7

---
+++ lesson-7/controller/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+class ProductController extends Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Определяем основной шаблон контроллера
+        $this->template = 'index.tpl';
+    }
+
+    public function index(){
+        $products = ProductModel::getLimitItem(25);
+        $this->vars['content'] = $this->twig->render('productContent.tpl',['products' => $products]);
+        return $this->renderOutput();
+    }
+
+
+    public function pagination($get){
+        $lastId = $get['lastId'];
+        $products = ProductModel::getPaginationProduct(25,$lastId);
+        $res = $this->twig->render('ajax/product.tpl',['products' => $products]);
+        $json = ['error' => false,'html' => $res];
+        echo json_encode($json);
+    }
+
+}
